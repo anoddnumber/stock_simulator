45bdff1cdc1913d27edb7a1baa313199167c33b4
Samson Wong <suchanoddnumber@gmail.com>
2015-08-29 16:36:55 -0700

    Fixed bug where create account form data would take it from the login form
    The form data had to be taken from the createAccountForm, since the
    names of the inputs of the login and create account form were the same,
    the jQuery selector would select the login inputs instead since it came
    first. Also added in client side validation that the password is the
    same as the retyped password when creating an account.

--- static/js/uiMethods.js
+++ static/js/uiMethods.js
@@ -16,17 +16,21 @@ $( document ).ready(function() {
     
     // process the create account form when pressing the submit button
     $('#createAccountForm').submit(function(event) {
-        
-        var formData = {
-            'username' : $('input[name=username]').val(),
-            'password' : $('input[name=password]').val(),
-            'email'    : $('input[name=email]').val(),
-        };
-        
-        createAccount(formData);
-
         // stop the form from submitting the normal way and refreshing the page
         event.preventDefault();
+        
+        var formData = {
+            'username' : $('#createAccountForm input[name=username]').val(),
+            'password' : $('#createAccountForm input[name=password]').val(),
+            'retypePassword' : $('#createAccountForm input[name=retypePassword]').val(),
+            'email'    : $('#createAccountForm input[name=email]').val(),
+        };
+        
+        if (formData.password !== formData.retypePassword) {
+            console.log("password does not match the retyped password");
+        } else {
+            createAccount(formData);
+        }
     });
     
     function createAccount(formData) {

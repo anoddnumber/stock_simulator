880f37526a21b919abf7d9e0670b145d1d2ea005
anoddnumber <suchanoddnumber@gmail.com>
2015-09-07 19:27:34 -0700

    Added comments

--- app.py
+++ app.py
@@ -8,10 +8,25 @@ from py.cache import Cache
 app = Flask(__name__, static_url_path='')
 cache = Cache()
 
+"""
+The root page. There is only 1 html page in this project.
+Everything else is a service.
+"""
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
-    
+
+"""
+Gets the stock prices of the passed in symbols.
+The client includes a symbols argument containing a string of stock symbols
+which the client wants the prices of. The symbols are delimited by commas (",").
+For example, symbols="AMZN,AAPL,GOOG" would be correct.
+
+The service has a cache where it looks for the data.
+The method attempts to update the cache every time it is called.
+The cache will update if it has been more than n minutes since it has updated (specified in the argument, currently 15 minutes).
+The stock prices will be returned in the same order as the arguments, delimited by newlines ("\n").
+"""
 @app.route("/info")
 def getStockInfo():
     cache.update(15)
@@ -22,11 +37,25 @@ def getStockInfo():
     symbols = [x.strip() for x in symbols.split(',')]
     return cache.getStockPrices(symbols)
 
+"""
+This service returns a json formatted file whose keys are available stock symbols
+and whose values are the stock symbols' names.
+
+The json file is generated from a file taken from NASDAQ (Which is updated daily).
+For more information on the file, visit the Wiki page on Github.
+
+TODO: retreive the NASDAQ file daily (currently called stock_symbols.txt) and generate the json file daily (currently called parsed_symbols.json).
+"""
 @app.route("/stockSymbolsMap", methods=['GET'])
 def getStockSymbolMap():
     print "getStockSymbolMap"
     return app.send_static_file('parsed_symbols.json')
 
+"""
+This service creates an account for the user.
+It takes in a username, password and email, does verfications, and saves the information to the database
+or raises an error if there is an issue.
+"""
 @app.route("/createAccount", methods=['POST'])
 def createAccount():
     username = request.form['username']
@@ -38,7 +67,11 @@ def createAccount():
     print "createAccount()"
     user = User(username, password, email)
     return userDbAccess.createUser(user)
-    
+
+"""
+Logs the user in. Verifies that the given username and password match the ones in the database.
+TODO: Cookies
+"""
 @app.route("/login", methods=['POST'])
 def login():
     print 'login'
--- py/cache.py
+++ py/cache.py
@@ -3,8 +3,15 @@ import json
 from yahoo_stock_api import YahooStockAPI
 import datetime
 
+"""
+The cache is responsible for having fast access to stock price information.
+It also allows clients to update it to make sure the information is not stale.
+The cache updates by calling the YahooStockApi and completely replaces the cache with the new information.
+"""
 class Cache:
-    
+    """
+    Creates the cache. If the path does not exist, create it by updating the cache.
+    """
     def __init__(self):
         #current directory is always /stock_simulator
         self.path = './static/cache.json' 
@@ -20,24 +27,42 @@ class Cache:
             print e
             self.update()
             
+    """
+    Returns the last datetime that the cache was updated.
+    Looks in the cache for the last_updated key and returns the value.
+    A sample datetime would be 2015-09-07 19:07:51.870404
+    """
     def getLastUpdatedDate(self):
         if self.parsed_json is None:
             return None
         last_updated_date = datetime.datetime.strptime(self.parsed_json['last_updated'], "%Y-%m-%d %H:%M:%S.%f")
         return last_updated_date
     
+    """
+    Updates the cache if timeInMinutes minutes have passed since the last time it was updated
+    or if there is no last updated date found (Either the cache does not exist or is corrupted).
+    
+    timeInMinutes - the number of minutes that should have passed before actually updating the cache
+    """
     def update(self, timeInMinutes = 0):
         last_updated_date = self.getLastUpdatedDate()
         if last_updated_date is not None:
             limit = datetime.timedelta(minutes = timeInMinutes)
-            difference = datetime.datetime.now() - self.getLastUpdatedDate()
+            difference = datetime.datetime.now() - last_updated_date
             if difference > limit:
                 self.updateCache()
         else:
             print 'no last_updated_date found'
             self.updateCache()
             
-            
+    """
+    Actually updates the cache by calling YahooStockAPI.
+    YahooStockAPI can only take 200 symbols at a time. Thus, break the requests to contain 200 or fewer symbols at a time.
+    Save the results in a new JSON object, append a last_updated key with the current time as the value, 
+    sort it alphabetically by keys, and store it in the cache at the end.
+    
+    TODO: make private, and refactor.
+    """
     def updateCache(self):
         #current directory is always /stock_simulator
         parsed_symbols_file = open('./static/parsed_symbols.json', 'r')
@@ -57,6 +82,7 @@ class Cache:
                 self.addResultsToNewCache(newJson, keysToSend, [x.strip() for x in results.split('\n')])
                 keysToSend = []
             keysToSend.append(key)
+            
         self.addResultsToNewCache(newJson, keysToSend, [x.strip() for x in results.split('\n')])
         newJson['last_updated'] = str(datetime.datetime.now())
         self.parsed_json = newJson
@@ -64,10 +90,25 @@ class Cache:
         cache = open(self.path, 'w')
         json.dump(newJson, cache, indent=1, sort_keys='true')
     
+    """
+    Builds the new JSON object with the given keys/results.
+    This JSON object represents the new cache.
+    
+    newJson - the JSON object to populate.
+    keys - an array of keys (strings) to be inserted into the JSON object.
+    results - an array of results (strings) corresponding to the given keys.
+    
+    TODO: make private
+    """
     def addResultsToNewCache(self, newJson, keys, results):
         for i, key in enumerate(keys):
             newJson[key] = results[i]
-            
+    
+    """
+    Returns stock prices that are delimited by newlines ("\n").
+    
+    symbols - an array of symbols whose stock prices will be returned
+    """
     def getStockPrices(self, symbols):
         prices = ''
         for symbol in symbols:

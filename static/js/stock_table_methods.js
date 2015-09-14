e55171ce8f86ed09203b4e6ddad5104692bc8929
anoddnumber <suchanoddnumber@gmail.com>
2015-09-13 22:10:54 -0700

    Buttons now hide/show
    Previous and Next buttons hide/show when appropriate now

--- static/js/stock_table_methods.js
+++ static/js/stock_table_methods.js
@@ -63,6 +63,7 @@ $( document ).ready(function() {
      * Displays all of the given stock symbols in a paginated view.
      * Meaning that the first n stocks will appear on the first page where n is config.numStocksPerPage
      * The other stocks will be displayed on the following pages when the user presses the next button.
+     * Updates the Previous and Next buttons at the end.
      * 
      * stockSymbols - an array of stock symbols (that are strings)
      */
@@ -78,6 +79,29 @@ $( document ).ready(function() {
             insertRowsBySymbols(displayedKeys);
         } else {
             insertRowByValue("Nothing found, please try again.");
+        }
+        
+        updateButtons();
+    }
+    
+    /**
+     * Hides/shows the Previous and Next buttons depending on if there are more pages to show to the left/right of the current page
+     */
+    function updateButtons() {
+        var firstPage = 0;
+        var lastPage = curSymbols.length/config.numStocksPerPage - 1;
+        if (firstPage === curPage) {
+            $('#previousStocksButton').hide();
+        } else {
+            $('#previousStocksButton').show();
+        }
+        
+        console.log(lastPage);
+        console.log(curPage);
+        if (lastPage <= curPage) {
+            $('#nextStocksButton').hide();
+        } else {
+            $('#nextStocksButton').show();
         }
     }
     
@@ -142,7 +166,8 @@ $( document ).ready(function() {
      * Moves to and displays the previous page of stocks.
      */
     $('#previousStocksButton').click(function() {
-        if (curPage > 0) {
+        var firstPage = 0;
+        if (curPage > firstPage) {
             curPage--;
             displayStocks(curSymbols);
         }
@@ -152,7 +177,8 @@ $( document ).ready(function() {
      * Moves to and displays the next page of stocks.
      */
     $('#nextStocksButton').click(function() {
-        if (curPage < curSymbols.length/config.numStocksPerPage - 1) {
+        var lastPage = curSymbols.length/config.numStocksPerPage - 1;
+        if (curPage < lastPage) {
             curPage++;
             displayStocks(curSymbols);
         }

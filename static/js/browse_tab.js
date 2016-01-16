4ff8c4bb518c3542ea7bd379ef39a6f9d547e617
anoddnumber <suchanoddnumber@gmail.com>
2016-01-15 23:38:17 -0800

    Stock table uses DataTables now
    No need to create custom functions to sort anymore.

--- static/js/browse_tab.js
+++ static/js/browse_tab.js
@@ -8,72 +8,9 @@ var config = {
 */
 (function($) {
     $.BrowseTab = function(options) {
-        var curSymbols; //the symbols that should be shown
-        var curPage = 0;
-        var lastUpdatedDate; //last time the server's cache was updated
-
         var browseTab = {
             options : $.extend({
             }, options),
-
-            getCurSymbols : function() {
-                return curSymbols;
-            },
-
-            getCurPage : function() {
-                return curPage;
-            },
-
-            getLastUpdatedDate : function() {
-                return lastUpdatedDate;
-            },
-
-            search : function() {
-                var query = $('#searchStocksForm input[name=searchbar]').val().trim();
-                var regEx = new RegExp(query,'i'); //i flag to ignore case
-                var keys = Object.keys(stockSymbolsMap);
-                var keysToShow = [];
-
-                keys.forEach(function(value, index, arr) {
-                    var info = stockSymbolsMap[value];
-                    var keysToSearch = ['daily_percent_change', 'daily_price_change', 'name', 'price'];
-                    var flag = false;
-
-                    for (var i = 0; i < keysToSearch.length; i++) {
-                        if (info[keysToSearch[i]].toString().match(regEx)) {
-                            flag = true;
-                            break;
-                        }
-                    }
-
-                    if (flag === true || value.match(regEx)) {
-                        keysToShow.push(value);
-                    }
-                });
-
-                curPage = 0;
-                if (browseTabTableSortManager) {
-                    browseTabTableSortManager.setSortedColumn($('#symbol-column-link').text());
-                    browseTabTableSortManager.setForward(true);
-                }
-                browseTab.displayStocks(keysToShow);
-            },
-
-            showNextPage : function() {
-                var lastPage = curSymbols.length/config.numStocksPerPage - 1;
-                if (curPage < lastPage) {
-                    curPage++;
-                    browseTab.displayStocks(curSymbols);
-                }
-            },
-
-            showPreviousPage : function() {
-                var firstPage = 0;
-                if (curPage > firstPage) {
-                    curPage--;
-                    browseTab.displayStocks(browseTab.getCurSymbols());
-                }
-            },
 
             /**
              * Displays all of the given stock symbols in a paginated view.
@@ -84,49 +21,14 @@ var config = {
              * stockSymbols - an array of stock symbols (that are strings)
              */
             displayStocks : function(stockSymbols) {
-                var begin = curPage * config.numStocksPerPage;
-                var end = (curPage + 1) * config.numStocksPerPage;
-                var displayedKeys = stockSymbols.slice(begin, end);
-                curSymbols = stockSymbols;
-
-                if (! browseTabTableSortManager) {
-                    browseTabTableSortManager = $.TableSortManager({
-                        data : Utility.filterByKeys(stockSymbolsMap, curSymbols),
-                        sortedColumn : $('#symbol-column-link').text(),
-                        forward : true,
-                    });
-                } else {
-                    browseTabTableSortManager.updateData(Utility.filterByKeys(stockSymbolsMap, curSymbols));
-                }
+                var oldTable = $('#stocks_table').dataTable();
+                oldTable.fnDestroy();
 
                 var tableBody = $('#stocks_table tbody')[0]; //grab the DOM element (0 indexed element of a jQuery object)
                 tableBody.innerHTML = "";
-                if (displayedKeys.length > 0) {
-                    browseTab.insertRowsBySymbols(displayedKeys);
-                } else {
-                    Utility.insertRowByValue('stocks_table tbody', "Nothing found, please try again.");
-                }
+                browseTab.insertRowsBySymbols(stockSymbols);
 
-                browseTab.updateButtons();
-            },
-
-            /**
-             * Hides/shows the Previous and Next buttons depending on if there are more pages to show to the left/right of the current page
-             */
-            updateButtons : function() {
-                var firstPage = 0;
-                var lastPage = curSymbols.length/config.numStocksPerPage - 1;
-                if (firstPage === curPage) {
-                    $('#previousStocksButton').hide();
-                } else {
-                    $('#previousStocksButton').show();
-                }
-
-                if (lastPage <= curPage) {
-                    $('#nextStocksButton').hide();
-                } else {
-                    $('#nextStocksButton').show();
-                }
+                $('#stocks_table').DataTable();
             },
 
              /**
@@ -170,55 +72,11 @@ var config = {
                 $('#previewBuyStockPrice').text(stockSymbolsMap[symbol].price);
             },
 
-            filterStocks : function(minPrice, maxPrice) {
-                if (maxPrice == undefined) {
-                    maxPrice = Number.MAX_VALUE;
-                }
-
-                var keys = Object.keys(stockSymbolsMap);
-                var filteredArray = [];
-
-                keys.forEach(function(value, index, arr) {
-                    var price = stockSymbolsMap[value].price;
-                    if (price >= minPrice && price <= maxPrice) {
-                        filteredArray.push(value);
-                    }
-                });
-
-                filteredArray.sort(function(a, b) {
-                    return stockSymbolsMap[a].price - stockSymbolsMap[b].price;
-                })
-
-                curPage = 0;
-                if (browseTabTableSortManager) {
-                    browseTabTableSortManager.setSortedColumn($('#symbol-column-link').text());
-                    browseTabTableSortManager.setForward(true);
-                }
-                browseTab.displayStocks(filteredArray);
-            },
-
-            displaySortedArray : function(sortedArray) {
-                var keysToShow = [];
-                for (var i = 0; i < sortedArray.length; i++) {
-                    keysToShow.push(sortedArray[i].stock_symbol);
-                }
-
-                curPage = 0;
-                browseTab.displayStocks(keysToShow);
-            },
-
             //TODO: add an update method for the browseTab
         }
 
         return {
-            getCurSymbols : browseTab.getCurSymbols,
-            getCurPage : browseTab.getCurPage,
-            getLastUpdatedDate : browseTab.getLastUpdatedDate,
-            showPreviousPage : browseTab.showPreviousPage,
-            showNextPage : browseTab.showNextPage,
-            search : browseTab.search,
             displayStocks : browseTab.displayStocks,
-            filterStocks : browseTab.filterStocks,
             displaySortedArray : browseTab.displaySortedArray,
         };
     };
@@ -226,24 +84,9 @@ var config = {
 
 
 var BrowseTab = $.BrowseTab();
-var browseTabTableSortManager;
 
 $( document ).ready(function() {
     ApiClient.updateCache();
-
-    /**
-     * Moves to and displays the previous page of stocks.
-     */
-    $('#previousStocksButton').click(function() {
-        BrowseTab.showPreviousPage();
-    });
-
-    /**
-     * Moves to and displays the next page of stocks.
-     */
-    $('#nextStocksButton').click(function() {
-        BrowseTab.showNextPage();
-    });
 
     /**
      * Computes the total amount of money required to buy the number of stocks
@@ -269,64 +112,7 @@ $( document ).ready(function() {
         }
     });
 
-    /**
-     * Finds out which stocks/stock names have matches with the searched value.
-     * Then displays those stocks with the queried stock information.
-     */
-    $("#stockSearchBar").keyup(function() {
-        BrowseTab.search();
-    });
-
-    /**
-     * Do nothing when the searchStocksForm is submitted.
-     * Searching should be done on the keyup event.
-     */
-    $('#searchStocksForm').submit(function(event) {
-        // stop the form from submitting the normal way and refreshing the page
-        event.preventDefault();
-    });
-
-    $("#symbol-column-link").click(function(e) {
-        e.preventDefault();
-        var sortedArray = browseTabTableSortManager.sort($('#symbol-column-link').text(), function(a, b) {
-            return a.stock_symbol.localeCompare(b.stock_symbol);
-        });
-        BrowseTab.displaySortedArray(sortedArray);
-    })
-
-    $("#name-column-link").click(function(e) {
-        e.preventDefault();
-        var sortedArray = browseTabTableSortManager.sort($('#name-column-link').text(), function(a, b) {
-            return a.stock_info.name.localeCompare(b.stock_info.name);
-        });
-        BrowseTab.displaySortedArray(sortedArray);
-    })
-
-    $("#daily-percent-change-column-link").click(function(e) {
-        e.preventDefault();
-        var sortedArray = browseTabTableSortManager.sort($('#daily-percent-change-column-link').text(), function(a, b) {
-            var one = a.stock_info.daily_percent_change.replace("%","");
-            var two = b.stock_info.daily_percent_change.replace("%","");
-            return Number(one) - Number(two);
-        });
-        BrowseTab.displaySortedArray(sortedArray);
-    })
-
-    $("#daily-price-change-column-link").click(function(e) {
-        e.preventDefault();
-        var sortedArray = browseTabTableSortManager.sort($('#daily-price-change-column-link').text(), function(a, b) {
-            return a.stock_info.daily_price_change - b.stock_info.daily_price_change;
-        });
-        BrowseTab.displaySortedArray(sortedArray);
-    })
-
-    $("#stock-column-link").click(function(e) {
-        e.preventDefault();
-        var sortedArray = browseTabTableSortManager.sort($('#stock-column-link').text(), function(a, b) {
-            return a.stock_info.price - b.stock_info.price;
-        });
-        BrowseTab.displaySortedArray(sortedArray);
-    })
+    $('#example').DataTable();
 });
 
 

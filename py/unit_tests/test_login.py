da07397498539462eca777e7bc837d9489239ef9
anoddnumber <suchanoddnumber@gmail.com>
2016-02-29 23:29:25 -0800

    Fixed/Added test case

--- py/unit_tests/test_login.py
+++ py/unit_tests/test_login.py
@@ -20,10 +20,18 @@ class TestLogin(unittest.TestCase):
         assert TestClient.is_simulator_page(rv.data)
         assert rv.status_code == 200
 
-    def test_login_bad_password(self):
-        print "\ntest_login_bad_password"
+    def test_login_without_account(self):
+        print "\ntest_login_without_account"
         rv = self.client.login()
         assert 'Email and password do not match up' in rv.data
+        assert rv.status_code == 200
+
+    def test_login_bad_password(self):
+        print "\ntest_login_bad_password"
+        self.client.create_account()
+        rv = self.client.login(password="bad_password")
+        assert TestClient.is_login_page(rv.data)
+        assert not TestClient.is_simulator_page(rv.data)
         assert rv.status_code == 200
 
     def test_login_nonexistent_email(self):

e6994f31e2a83947668cdb255af908fadfa6da05
anoddnumber <suchanoddnumber@gmail.com>
2016-03-06 08:49:31 -0800

    Test buy_as_much_as_possible

--- py/unit_tests/test_buy_stock.py
+++ py/unit_tests/test_buy_stock.py
@@ -32,6 +32,28 @@ class TestBuyStock(BaseUnitTest):
         price = price.replace(".", "_")
         assert user_dict.get('stocks_owned').get(symbol).get(price) == 1
 
+    def test_buy_as_much_as_possible(self):
+        print "test_buy_as_much_as_possible"
+        self.client.create_account()
+
+        symbol = "AMZN"
+
+        rv = self.client.get_stock_info(symbol)
+        price = rv.data
+
+        user_dict = self.assert_user_info({}, simulator.config.get("defaultCash"))
+        starting_cash = float(user_dict.get('cash'))
+
+        self.assert_user_info({}, starting_cash)
+
+        max_quantity_possible = int(starting_cash / float(price))
+        rv = self.client.buy_stock(symbol, max_quantity_possible, price)
+
+        assert "Success" in rv.data
+
+        self.assert_user_info({"AMZN" : {price.replace(".", "_") : max_quantity_possible} },
+                              starting_cash - max_quantity_possible * float(price))
+
     def test_buy_without_account(self):
         print "test_buy_without_account"
         symbol = "AMZN"
@@ -61,8 +83,8 @@ class TestBuyStock(BaseUnitTest):
         self.assert_user_info({}, starting_cash)
 
         # too much quantity
-        quantity = int(starting_cash / float(price) + 1)
-        rv = self.client.buy_stock(symbol, quantity, price)
+        too_much_quantity = int(starting_cash / float(price) + 1)
+        rv = self.client.buy_stock(symbol, too_much_quantity, price)
 
         assert "Not enough cash" in rv.data
 

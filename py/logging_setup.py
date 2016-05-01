73cd3ff7484c573027d9914ef626b54cbe0547ab
anoddnumber <suchanoddnumber@gmail.com>
2016-04-30 21:12:23 -0700

    Generate logs directory if not found

--- py/logging_setup.py
+++ py/logging_setup.py
@@ -2,24 +2,24 @@ import os
 import json
 import logging.config
 
-def setup(
-    default_path='./config/logging.json',
-    default_level=logging.INFO,
-    env_key='LOG_CFG'
-):
-    """Setup logging configuration
-
+def setup(path='./config/logging.json', level=logging.INFO, env_key='LOG_CFG',
+          log_dir='./logs'):
     """
-    path = default_path
+        Setup logging configuration
+    """
     value = os.getenv(env_key, None)
     if value:
         print "value"
         path = value
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
     if os.path.exists(path):
-        print "path exists"
+        print "path to logging config exists"
         with open(path, 'rt') as f:
             config = json.load(f)
         logging.config.dictConfig(config)
     else:
         print "path does not exist"
-        logging.basicConfig(level=default_level) #prints to standard out
+        logging.basicConfig(level=level) #prints to standard out

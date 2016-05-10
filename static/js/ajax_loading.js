a930c484ed8e15c74dd6ec4e3b429900ff4f13e9
Samson Wong <suchanoddnumber@gmail.com>
2016-05-09 23:46:10 -0700

    Forgot a file in the last commit..

--- static/js/ajax_loading.js
+++ static/js/ajax_loading.js
@@ -2,7 +2,7 @@
     $.ChangePageHelper = function(options) {
 
         var changePageHelper = {
-            attachChangePageAction : function (selector, page, tabId) {
+            attachChangePageAction : function (selector, page) {
                 selector.loadingbar({
                     target: "#loadingbar-frame",
                     replaceURL: true,
@@ -23,12 +23,6 @@
 
                         $(this.target).html(simulator.get(0));
                         page.init();
-
-                        if (tabId != '') {
-                            $('#navbarTabs > li').removeClass('active');
-                            $('#' + tabId).addClass('active');
-                        }
-
                     }
                 });
             }
@@ -43,7 +37,6 @@
 var ChangePageHelper = $.ChangePageHelper();
 
 $( document ).ready(function() {
-    ChangePageHelper.attachChangePageAction($(".stocksTabLink"), BrowseTab, 'stocksTab');
-    ChangePageHelper.attachChangePageAction($(".profileTabLink"), ProfileTab, 'profileTab');
-//    changePageHelper($(".profileTabLink"), StockInfoPage, '');
+    ChangePageHelper.attachChangePageAction($(".stocksTabLink"), BrowseTab);
+    ChangePageHelper.attachChangePageAction($(".profileTabLink"), ProfileTab);
 });

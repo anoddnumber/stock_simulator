71744f79fbe5a5f2f8684a67675093b1b0d9ee53
Samson Wong <suchanoddnumber@gmail.com>
2016-07-08 09:38:12 -0700

    Add in constants directory/files

---
+++ py/constants/errors.py
@@ -0,0 +1,4 @@
+UNEXPECTED_ERROR = 'An error has occurred. Please try selling the stocks again.'
+STOCK_DOES_NOT_EXIST = 'The stock you are trying to sell does not exist in our database.'
+PRICE_CHANGED = 'The stock price has changed, please try selling the stocks again.'
+

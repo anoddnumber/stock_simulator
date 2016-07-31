4dbed3e14ea3100cd9b011e3e7738f195f449ef5
Samson Wong <suchanoddnumber@gmail.com>
2016-07-31 14:38:02 -0700

    Make ChangePageHelper easy to use
    All a elements with an href attribute will automatically use the change
    youtube-like-loading-bar

--- static/js/ajax_loading.js
+++ static/js/ajax_loading.js
@@ -7,27 +7,30 @@
                     replaceURL = true;
                 }
 
-                selector.unbind("click");
-                selector.loadingbar({
-                    target: "#loadingbar-frame",
-                    replaceURL: replaceURL,
-                    direction: "right",
+                for (var i = 0; i < selector.length; i++) {
+                    value = $(selector.get(i));
+                    value.unbind("click");
+                    value.loadingbar({
+                        target: "#loadingbar-frame",
+                        replaceURL: replaceURL,
+                        direction: "right",
 
-                    /* Default Ajax Parameters.  */
-                    async: true,
-                    complete: function(xhr, text) {},
-                    cache: true,
-                    error: function(xhr, text, e) {},
-                    global: true,
-                    headers: {},
-                    statusCode: {},
-                    success: function(data, text, xhr) {},
-                    dataType: "html",
-                    done: function(data) {
-                        var simulator = $(data).find("#stock_simulator");
-                        $(this.target).html(simulator);
-                    }
-                });
+                        /* Default Ajax Parameters.  */
+                        async: true,
+                        complete: function(xhr, text) {},
+                        cache: true,
+                        error: function(xhr, text, e) {},
+                        global: true,
+                        headers: {},
+                        statusCode: {},
+                        success: function(data, text, xhr) {},
+                        dataType: "html",
+                        done: function(data) {
+                            var simulator = $(data).find("#stock_simulator");
+                            $(this.target).html(simulator);
+                        }
+                    });
+                }
             }
         }
 
@@ -40,6 +43,6 @@
 var ChangePageHelper = $.ChangePageHelper();
 
 $( document ).ready(function() {
-    ChangePageHelper.attachChangePageAction($(".stocksPageLink"));
-    ChangePageHelper.attachChangePageAction($(".profilePageLink"));
+    // attach the page helper to all a elements with attribute href
+    ChangePageHelper.attachChangePageAction($('a[href]'));
 });

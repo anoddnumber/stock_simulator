93fb5498f783b82c88d2aa2e2dbfb744214c5d02
Samson Wong <suchanoddnumber@gmail.com>
2016-08-16 09:22:15 -0700

    Implement find transactions

--- py/datastores/transaction_datastore.py
+++ py/datastores/transaction_datastore.py
@@ -3,8 +3,17 @@ from py.user import Transaction
 
 
 class MongoEngineTransactionDatastore(MongoEngineDatastore):
-    def find_transactions(self, username):
-        pass
+    def find_transactions(self, user_id):
+        """
+        :param user_id: The user's ID (has type ObjectId)
+        :return: A list of transaction Objects associated with the user
+        """
+        from mongoengine import ValidationError
+        try:
+            return Transaction.objects(user_id=user_id)
+        except ValidationError:
+            # TODO: is there a way to handle this? look at MongoEngineUserDatastore
+            return None
 
     def create_transaction(self, user_id, properties):
         transaction = Transaction(user_id=user_id, properties=properties)
